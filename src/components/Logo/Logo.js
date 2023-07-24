c73d4c62ed46d307462ce09301b2994fd3b541c5
ksvinothkumar <vinothraj005@gmail.com>
2023-07-24 07:34:01 +0530

    'intial'

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,25 @@
+// material-ui
+import logo from './CNA_logo.png'; // with import
+/**
+ * if you want to use image instead of <svg> uncomment following.
+ *
+ * import logoDark from 'assets/images/logo-dark.svg';
+ * import logo from 'assets/images/logo.svg';
+ *
+ */
+
+// ==============================|| LOGO SVG ||============================== //
+
+const Logo = () => {
+  return (
+    /**
+     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
+     *
+     * <img src={logo} alt="Mantis" width="100" />
+     *
+     */
+    <img src={logo} alt="CNA logo" width="50%" />
+  );
+};
+
+export default Logo;
